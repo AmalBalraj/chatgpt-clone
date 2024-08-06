99bac6303751c37b88b8fb36fcbac957bdde83e7
AmalBalraj <amalbalraj99@gmail.com>
2024-08-06 20:04:54 +0530

    changes

---
+++ src/routes/signInPage/SignInPage.jsx
@@ -0,0 +1,10 @@
+import './signInPage.css'
+import { SignIn } from "@clerk/clerk-react"
+
+const SignInPage = () => {
+  return (
+    <div className='signInPage'><SignIn path="/sign-in" signUpUrl='/sign-up'/></div>
+  )
+}
+
+export default SignInPage
