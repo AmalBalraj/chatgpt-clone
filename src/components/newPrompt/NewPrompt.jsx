0e0c2f4ff64dd611a27aeb468153d4e7ab4568e9
AmalBalraj <amalbalraj99@gmail.com>
2024-08-28 21:06:07 +0530

    New prompt changes, chat POST request

---
+++ src/components/newPrompt/NewPrompt.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from "react";
+import "./newPrompt.css";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
+
+const NewPrompt = () => {
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [img, setImg] = useState({
+        isLoading: false,
+        error: "",
+        dbData: {},
+        aiData: {},
+    });
+
+    const chat = model.startChat({
+        history: [
+            {
+                role: "user",
+                parts: [{ text: "Hello" }],
+            },
+            {
+                role: "model",
+                parts: [
+                    { text: "Great to meet you. What would you like to know?" },
+                ],
+            },
+        ],
+        generationConfig: {
+            //maxOutputTokens:100,
+        },
+    });
+
+    const endRef = useRef(null);
+
+    useEffect(() => {
+        endRef.current.scrollIntoView({ behavior: "smooth" });
+    }, [question, answer, img.dbData]);
+
+    const add = async (prompt) => {
+        setQuestion(prompt);
+
+        const result = await chat.sendMessageStream(
+            Object.entries(img.aiData).length ? [img.aiData, prompt] : [prompt]
+        );
+        let accumText = "";
+        for await (const chunk of result.stream) {
+            const chunkText = chunk.text();
+            accumText += chunkText;
+            setAnswer(accumText);
+        }
+        setImg({
+            isLoading: false,
+            error: "",
+            dbData: {},
+            aiData: {},
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const text = e.target.text.value;
+        if (!text) return;
+
+        add(text)
+    };
+
+    return (
+        <>
+            {/*add new chat*/}
+            {img.isLoading && <div className="">Loading...</div>}
+            {img.dbData?.filePath && (
+                <IKImage
+                    urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                    path={img.dbData?.filePath}
+                    width="380"
+                    transformation={[{ width: 380 }]}
+                />
+            )}
+            {console.log(question)}
+            {question && <div className="message user">{question}</div>}
+            {answer && (
+                <div className="message">
+                    <Markdown>{answer}</Markdown>
+                </div>
+            )}
+            <div className="endChat" ref={endRef}></div>
+            <form className="newForm" onSubmit={handleSubmit}>
+                <Upload setImg={setImg} />
+                <input id="file" type="file" multiple={false} hidden />
+                <input type="text" name="text" placeholder="Ask anything..." />
+                <button>
+                    <img src="/arrow.png" alt="" />
+                </button>
+            </form>
+        </>
+    );
+};
+
+export default NewPrompt;
--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,54 +1,56 @@
-import './chatPage.css'
+import "./chatPage.css";
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
-  return (
-    <div className='chatPage'>
-      <div className="wrapper">
-        <div className="chat">
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
-          <div className="message">text from ai</div>
-          <div className="message user">text from user</div>
+    return (
+        <div className="chatPage">
+            <div className="wrapper">
+                <div className="chat">
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <div className="message">text from ai</div>
+                    <div className="message user">text from user</div>
+                    <NewPrompt />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default ChatPage
+export default ChatPage;
